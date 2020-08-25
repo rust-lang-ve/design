ea1ce64f25d3e2c13d0ceb032e89b2d6ad021529
Esteban Borai <estebanborai@gmail.com>
2020-08-24 21:13:17 -0300

    init gh pages

--- layout/src/index.js
+++
@@ -1,8 +0,0 @@
-import './style/main.scss';
-const template = require('./markup/index.hbs');
-
-document.addEventListener("DOMContentLoaded", function() {
-  const divEl = document.createElement('div');
-  divEl.innerHTML = template();
-  document.body.appendChild(div);
-});
